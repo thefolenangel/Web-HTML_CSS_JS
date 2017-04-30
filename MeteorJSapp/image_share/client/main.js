99705e1ffffcb6b2502bfc2827fab7e828b6cd55
Ivan Penchev <thefolenangel@gmail.com>
2017-04-30 13:12:53 +0200

    Added DB to store the images, added rating functionality, added deletion ability.

--- MeteorJSapp/image_share/client/main.js
+++ MeteorJSapp/image_share/client/main.js
@@ -1,22 +1,57 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Images = new Mongo.Collection("images");
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+ Template.images.helpers({images:
+    Images.find({}, {sort:{createdOn: -1, rating:-1}})
+  });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+   Template.images.events({
+    'click .js-image':function(event){
+        $(event.target).css("width", "50px");
+    }, 
+    'click .js-del-image':function(event){
+       var image_id = this._id;
+       console.log(image_id);
+       // use jquery to hide the image component
+       // then remove it at the end of the animation
+       $("#"+image_id).hide('slow', function(){
+        Images.remove({"_id":image_id});
+       })  
+    }, 
+    'click .js-rate-image':function(event){
+      var rating = $(event.currentTarget).data("userrating");
+      console.log(rating);
+      var image_id = this.id;
+      console.log(image_id);
+
+      Images.update({_id:image_id}, 
+                    {$set: {rating:rating}});
+    }, 
+    'click .js-show-image-form':function(event){
+      $("#image_add_form").modal('show');
+    }
+
+   });
+
+  Template.image_add_form.events({
+    'submit .js-add-image':function(event){
+      var img_src, img_alt;
+      img_src = event.target.img_src.value;
+      img_alt = event.target.img_alt.value;
+      console.log("src: "+img_src+" alt:"+img_alt);
+
+      Images.insert({
+        img_src:img_src, 
+        img_alt:img_alt, 
+        createdOn:new Date()
+      });
+       $("#image_add_form").modal('show');
+      return false;
+    }
+  });
+
--- MeteorJSapp/image_share/server/main.js
+++ MeteorJSapp/image_share/server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-
+Images = new Mongo.Collection("images");
 Meteor.startup(() => {
-  // code to run on server at startup
+  	Meteor.startup(function(){
+		if (Images.find().count() == 0){		
+			for (var i=1;i<23;i++){
+				Images.insert(
+					{
+      				img_src:"img_"+i+".jpg",
+      				img_alt:"image number "+i 
+   					}
+				);	
+			}// end of for insert images
+			// count the images!
+			console.log("startup.js says: "+Images.find().count());
+		}// end of if have no images
+	});
 });
