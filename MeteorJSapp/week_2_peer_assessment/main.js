f776c4a2bb8172fc663627f2e7bc8b65f9553fcc
Ivan Penchev <thefolenangel@gmail.com>
2017-05-07 22:02:27 +0200

    Added homework for week 2

--- MeteorJSapp/week_2_peer_assessment/main.js
+++ MeteorJSapp/week_2_peer_assessment/main.js
@@ -8,10 +8,20 @@ if (Meteor.isClient){
         // this runs when they click the add button... you need to compete it
      'click .js-add-site':function(event){
          var url = $('#url_input').val();// get the form value using jquery...
-         var site = {"url":url};// create a simple object to insert to the collectoin
-         console.log(site);
-         //Websites.insert(site);
-         // put your code in here...
+         var  user = "";
+         //test if there is a logged user that submits the data
+         if(Meteor.user()){
+             //if there is push his email to the DB
+             user = Meteor.user().emails[0].address;
+         } else{
+             //else push that its anonnymous
+             user = "anon";
+         }
+
+         var site = {"url":url,
+                     "createdOn":new Date(),
+                     "createdBy":user};
+         Websites.insert(site);
          return false;
      }
     });
@@ -21,6 +31,14 @@ if (Meteor.isClient){
         'all_websites':function(){
             return Websites.find({});
         },
+        'safer_email':function(email){
+            if (email.indexOf('@')!=-1){// we have an email
+                return email.split('@')[0];
+            }
+            else{// probably anonymouse.
+                return email;
+            }
+        }
     });
 
 }
