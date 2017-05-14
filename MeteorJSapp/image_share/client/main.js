89725c692a089673fca325982592395757508215
Ivan Penchev <thefolenangel@gmail.com>
2017-05-14 12:31:32 +0200

    Added image filtration by uploader

--- MeteorJSapp/image_share/client/main.js
+++ MeteorJSapp/image_share/client/main.js
@@ -6,8 +6,77 @@ import './main.html';
 
 Images = new Mongo.Collection("images");
 
- Template.images.helpers({images:
-    Images.find({}, {sort:{createdOn: -1, rating:-1}})
+Session.set("imageLimit", 8);
+
+  lastScrollTop = 0; 
+  $(window).scroll(function(event){
+    // test if we are near the bottom of the window
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+      // where are we in the page? 
+      var scrollTop = $(this).scrollTop();
+      // test if we are going down
+      if (scrollTop > lastScrollTop){
+        // yes we are heading down...
+       Session.set("imageLimit", Session.get("imageLimit") + 4);
+      }
+
+      lastScrollTop = scrollTop;
+    }
+        
+  })
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+  });
+
+Template.images.helpers({
+    images:function(){
+      if (Session.get("userFilter")){// they set a filter!
+        return Images.find({createdBy:Session.get("userFilter")}, {sort:{createdOn: -1, rating:-1}});         
+      }
+      else {
+        return Images.find({}, {sort:{createdOn: -1, rating:-1}, limit:Session.get("imageLimit")});         
+      }
+    },
+    filtering_images:function(){
+      if (Session.get("userFilter")){// they set a filter!
+        return true;
+      } 
+      else {
+        return false;
+      }
+    },
+    getFilterUser:function(){
+      if (Session.get("userFilter")){// they set a filter!
+        var user = Meteor.users.findOne(
+          {_id:Session.get("userFilter")});
+        return user.username;
+      } 
+      else {
+        return false;
+      }
+    },
+    getUser:function(user_id){
+      var user = Meteor.users.findOne({_id:user_id});
+      if (user){
+        return user.username;
+      }
+      else {
+        return "anon";
+      }
+    }
+  });
+
+  Template.body.helpers({username:function(){
+    if (Meteor.user()){
+      return Meteor.user().username;
+	  console.log(Meteor.user().username);
+        //return Meteor.user().emails[0].address;
+    }
+    else {
+      return "anonymous internet user";
+    }
+  }
   });
 
    Template.images.events({
@@ -34,7 +103,13 @@ Images = new Mongo.Collection("images");
     }, 
     'click .js-show-image-form':function(event){
       $("#image_add_form").modal('show');
-    }
+    }, 
+    'click .js-set-image-filter':function(event){
+        Session.set("userFilter", this.createdBy);
+    }, 
+    'click .js-unset-image-filter':function(event){
+        Session.set("userFilter", undefined);
+    }, 
 
    });
 
@@ -43,12 +118,13 @@ Images = new Mongo.Collection("images");
       var img_src, img_alt;
       img_src = event.target.img_src.value;
       img_alt = event.target.img_alt.value;
-      console.log("src: "+img_src+" alt:"+img_alt);
+      console.log("src: "+img_src+" alt:"+img_alt + "created " +Meteor.user()._id);
 
       Images.insert({
         img_src:img_src, 
         img_alt:img_alt, 
-        createdOn:new Date()
+        createdOn:new Date(),
+		 createdBy:Meteor.user()._id
       });
        $("#image_add_form").modal('show');
       return false;
